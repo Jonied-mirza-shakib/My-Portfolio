5b410f5fa93cf0932a9adcee8bca72fb28555c2a
Jonied-mirza-shakib <MDSHAKIB01088@gmail.com>
2022-06-04 16:31:06 +0600

    updated the readme file

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -14,14 +14,14 @@ const Home = () => {
                 <div className='row'>
                     <div className="col-lg-6">
                         <div className='first-section'>
-                            <h1 style={{ color: 'dimgray' }}><span>Web Developer</span>
+                            <h1 style={{ color: 'dimgray', marginBottom: '10px' }}><span>Web Developer</span>
                                 <br />
                                 <span>Mission 2022</span></h1>
                         </div>
-                        <h2 style={{ color: 'dimgray' }}>HELLO!</h2>
-                        <h2 style={{ color: 'dimgray' }}>I AM JONIEAD MIRZA SHAKIB</h2>
-                        <p style={{ fontWeight: 'bold', color: 'darkblue' }}>I Am Junior MERN-Stack Web Devloper</p>
-                        <a className='btn btn-outline-primary' href="https://drive.google.com/uc?export=download&id=1kQLwspXeiKHe9ELTpkCBjtVrtC8GZGN3" target='_blank'> <span style={{ fontSize: '25px' }}><ImMan /></span> Download My Resume</a>
+                        <h2 style={{ color: 'dimgray', marginBottom: '10px' }}>HELLO!</h2>
+                        <h2 style={{ color: 'dimgray', marginBottom: '10px' }}>I AM JONIEAD MIRZA SHAKIB</h2>
+                        <p style={{ fontWeight: 'bold', color: 'darkblue', marginBottom: '20px' }}>I Am Junior MERN-Stack Web Devloper</p>
+                        <a style={{ fontSize: '15px', fontWeight: 'bold' }} className='btn btn-primary rounded' href="https://drive.google.com/uc?export=download&id=1kQLwspXeiKHe9ELTpkCBjtVrtC8GZGN3" target='_blank'> <span style={{ fontSize: '25px' }}><ImMan /></span> Download My Resume</a>
                     </div>
                     <div className="col-lg-6">
                         <img style={{ width: '70%' }} src={img} alt="" />
